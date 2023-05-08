c9e3d82485592b841898b5d2bb02c469047cc7b1
shihaim <hashi00518@gmail.com>
2023-05-08 19:32:20 +0900

    [dev branch] 5/8

--- tennis-store/src/test/java/com/tnc/study/tennisstore/QueryDSLTest.java
+++ tennis-store/src/test/java/com/tnc/study/tennisstore/QueryDSLTest.java
@@ -2,6 +2,12 @@ package com.tnc.study.tennisstore;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tnc.study.tennisstore.domain.Address;
 import com.tnc.study.tennisstore.domain.Email;
@@ -10,6 +16,7 @@ import com.tnc.study.tennisstore.domain.Password;
 import com.tnc.study.tennisstore.domain.member.Member;
 import com.tnc.study.tennisstore.domain.member.MemberGrade;
 import com.tnc.study.tennisstore.domain.member.QMember;
+import com.tnc.study.tennisstore.domain.order.*;
 import com.tnc.study.tennisstore.domain.product.Product;
 import com.tnc.study.tennisstore.domain.product.QProduct;
 import com.tnc.study.tennisstore.domain.product.ball.Ball;
@@ -18,7 +25,9 @@ import com.tnc.study.tennisstore.domain.product.racquet.Racquet;
 import com.tnc.study.tennisstore.domain.product.shoes.GroundType;
 import com.tnc.study.tennisstore.domain.product.shoes.Shoes;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PersistenceUnit;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -27,9 +36,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.tnc.study.tennisstore.domain.member.QMember.*;
+import static com.tnc.study.tennisstore.domain.order.QOrder.*;
+import static com.tnc.study.tennisstore.domain.order.QOrderLine.*;
 import static com.tnc.study.tennisstore.domain.product.QProduct.*;
 import static org.assertj.core.api.Assertions.*;
 
@@ -40,6 +54,9 @@ public class QueryDSLTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
 
     JPAQueryFactory queryFactory;
 
@@ -115,6 +132,22 @@ public class QueryDSLTest {
             em.persist(racquet);
             em.persist(shoes);
             em.persist(ball);
+
+            List<OrderLine> orderLines1 = Arrays.asList(
+                    new OrderLine(racquet, racquet.getPrice(), i),
+                    new OrderLine(shoes, shoes.getPrice(), i)
+            );
+            List<OrderLine> orderLines2 = List.of(
+                    new OrderLine(ball, ball.getPrice(), i)
+            );
+
+            Receiver receiver1 = new Receiver(member.getName(), "010-4461-0579");
+            Receiver receiver2 = new Receiver(member3.getName(), "010-4461-0579");
+
+            Order order1 = new Order(member, orderLines1, member.getAddress(), receiver1, "빨리 빨리 와주세요" + i);
+            Order order2 = new Order(member3, orderLines2, member3.getAddress(), receiver2, "안전하게 와주세요" + i);
+            em.persist(order1);
+            em.persist(order2);
         }
     }
 
@@ -240,7 +273,6 @@ public class QueryDSLTest {
         BigDecimal min = tuple.get(product.price.amount.min());
 
         assertThat(count).isEqualTo(300);
-        System.out.println("sum = " + sum);
         assertThat(sum.compareTo(BigDecimal.valueOf(50_215_150)) == 0).isTrue();
         assertThat(max.compareTo(BigDecimal.valueOf(350_100)) == 0).isTrue();
         assertThat(min.compareTo(BigDecimal.valueOf(3001)) == 0).isTrue();
@@ -273,5 +305,284 @@ public class QueryDSLTest {
         assertThat(tuple1Avg).isEqualTo(350_050.5);
         assertThat(tuple2Brand).isEqualTo("나이키");
         assertThat(tuple2Avg).isEqualTo(149_050.5);
+    }
+
+    @Test
+    @DisplayName("조인")
+    void test9() {
+        em.flush();
+        em.clear();
+
+        List<Order> orders = queryFactory
+                .selectFrom(order)
+                .join(order.member, member)
+                .where(member.name.eq("MJK"))
+                .fetch();
+
+        // false면 아직 로딩 전 / true면 로딩된 상태
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(orders.get(0).getMember());
+
+        assertThat(loaded).isFalse();
+//        Order order1 = orders.get(0);
+//        assertThat(order1.getOrderLines().get(0).getProduct().getName()).isEqualTo("공 1");
+    }
+
+    @Test
+    @DisplayName("조인 - fetch join")
+    void test10() {
+        em.flush();
+        em.clear();
+
+        List<Order> orders = queryFactory
+                .selectFrom(order)
+                .join(order.member, member).fetchJoin()
+                .where(member.name.eq("MJK"))
+                .fetch();
+
+        // false면 아직 로딩 전 / true면 로딩된 상태
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(orders.get(0).getMember());
+
+        assertThat(loaded).isTrue();
+//        Order order1 = orders.get(0);
+//        assertThat(order1.getOrderLines().get(0).getProduct().getName()).isEqualTo("공 1");
+    }
+
+    @Test
+    @DisplayName("서브 쿼리 - Where")
+    void test11() {
+        QProduct productSub = new QProduct("productSub");
+
+        Product findProduct = queryFactory
+                .selectFrom(product)
+                .where(product.price.amount.eq(
+                        JPAExpressions
+                                .select(productSub.price.amount.max())
+                                .from(productSub)
+                ))
+                .fetchOne();
+
+        assertThat(findProduct.getName()).isEqualTo("라켓 100");
+        assertThat(findProduct.getPrice()).isEqualTo(Money.of(350_100L));
+    }
+
+    @Test
+    @DisplayName("서브 쿼리 - Select")
+    void test12() {
+
+        JPQLQuery<BigDecimal> orderTotalPriceExpression = JPAExpressions
+                .select(orderLine.orderPrice.amount.sum())
+                .from(order)
+                .join(orderLine)
+                .on(order.id.eq(orderLine.order.id))
+                .where(member.id.eq(order.member.id));
+
+        List<Tuple> result = queryFactory
+                .select(member.name, orderTotalPriceExpression)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String memberName = tuple.get(member.name);
+            BigDecimal orderTotalPrice = tuple.get(orderTotalPriceExpression);
+
+            System.out.printf("%s : %s\n", memberName, (orderTotalPrice != null ? orderTotalPrice : BigDecimal.ZERO));
+
+        }
+    }
+
+    @Test
+    @DisplayName("서브 쿼리 - From (inline view)")
+    void test13() {
+        List<jakarta.persistence.Tuple> resultList = em.createQuery("""
+                            select t.name, t.fullAddress
+                            from (
+                               select m.name as name,
+                                      m.address.address1 || ' ' || m.address.address2 || ' ' || m.address.zipcode as fullAddress
+                               from Member m
+                            ) t
+                        """, jakarta.persistence.Tuple.class)
+                .getResultList();
+
+        for (jakarta.persistence.Tuple tuple : resultList) {
+            String memberName = tuple.get(0, String.class);
+            String fullAddress = tuple.get(1, String.class);
+            System.out.println(memberName + ": " + fullAddress);
+        }
+    }
+
+    @Test
+    @DisplayName("Case 문 - 단순한 조건")
+    void test14() {
+        List<String> result = queryFactory
+                .select(member.name
+                        .when("김명진").then("한글")
+                        .when("MJK").then("영어")
+                        .otherwise("하하하하"))
+                .from(member)
+                .orderBy(member.name.asc()).fetch();
+
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result).containsExactly("영어", "한글", "한글");
+    }
+
+    @Test
+    @DisplayName("Case 문 - 복잡한 조건")
+    void test15() {
+        List<String> result = queryFactory
+                .select(new CaseBuilder()
+                        .when(product.price.amount.between(0, 100_000)).then("저렴")
+                        .when(product.price.amount.between(100_000, 200_000)).then("적당")
+                        .otherwise("비쌈")
+                )
+                .from(product)
+                .fetch();
+
+        Map<String, List<String>> collect = result.stream()
+                .collect(Collectors.groupingBy(str -> str));
+
+        assertThat(result.size()).isEqualTo(300);
+        assertThat(collect.size()).isEqualTo(3);
+        assertThat(collect.get("저렴").size()).isEqualTo(100);
+        assertThat(collect.get("적당").size()).isEqualTo(100);
+        assertThat(collect.get("비쌈").size()).isEqualTo(100);
+    }
+
+    @Test
+    @DisplayName("Case 문 - Order by절에서 사용")
+    void test16() {
+
+        NumberExpression<Integer> priceRank = new CaseBuilder()
+                .when(product.price.amount.between(0, 100_000)).then(2)
+                .when(product.price.amount.between(100_000, 200_000)).then(1)
+                .otherwise(3);
+
+        List<Tuple> result = queryFactory
+                .select(product.name, product.price.amount, priceRank)
+                .from(product)
+                .orderBy(priceRank.asc())
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String productName = tuple.get(product.name);
+            BigDecimal price = tuple.get(product.price.amount);
+            Integer rank = tuple.get(priceRank);
+
+            System.out.println(productName + ": " + price + ": " + rank);
+        }
+    }
+
+    @Test
+    @DisplayName("상수 사용")
+    void test17() {
+
+        // 자바에서 적용
+        Tuple result = queryFactory
+                .select(member.name, Expressions.constant("오늘 점심 뭐 먹지?"))
+                .from(member)
+                .where(member.name.eq("MJK"))
+                .fetchOne();
+
+        String memberName = result.get(member.name);
+        String constant = result.get(Expressions.constant("오늘 점심 뭐 먹지?"));
+
+        assertThat(memberName).isEqualTo("MJK");
+        assertThat(constant).isEqualTo("오늘 점심 뭐 먹지?");
+    }
+
+    @Test
+    @DisplayName("문자 더하기")
+    void test18() {
+        String result = queryFactory
+                .select(member.name.concat("_").concat(member.grade.stringValue()))
+                .from(member)
+                .where(member.name.eq("MJK"))
+                .fetchOne();
+
+        assertThat(result).isEqualTo("MJK_GOLD");
+    }
+
+    @Test
+    @DisplayName("Querydsl 빈 생성 - setter")
+    void test19() {
+        List<MemberDto> result = queryFactory
+                .select(Projections.bean(MemberDto.class,
+                        member.name,
+                        member.grade,
+                        member.address.address1
+                                .concat(" ")
+                                .concat(member.address.address2)
+                                .concat(" ")
+                                .concat(member.address.zipcode).as("fullAddress")
+                ))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 빈 생성 - Field")
+    void test20() {
+        List<MemberDto> result = queryFactory
+                .select(Projections.fields(MemberDto.class,
+                        member.name,
+                        member.grade,
+                        member.address.address1
+                                .concat(" ")
+                                .concat(member.address.address2)
+                                .concat(" ")
+                                .concat(member.address.zipcode).as("fullAddress")
+                ))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 빈 생성 - Constructor")
+    void test21() {
+        List<MemberDto> result = queryFactory
+                .select(Projections.constructor(MemberDto.class,
+                        member.name,
+                        member.address.address1
+                                .concat(" ")
+                                .concat(member.address.address2)
+                                .concat(" ")
+                                .concat(member.address.zipcode).as("fullAddress"),
+                        member.grade
+                ))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
+    }
+
+    @Test
+    @DisplayName("Querydsl 빈 생성 - QueryProjection")
+    void test22() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(
+                        member.name,
+                        member.address.address1
+                                .concat(" ")
+                                .concat(member.address.address2)
+                                .concat(" ")
+                                .concat(member.address.zipcode).as("fullAddress"),
+                        member.grade
+                ))
+                .from(member)
+                .fetch();
+
+        // then
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
+        }
     }
 }
